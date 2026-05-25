e6ff2372cf849d44b2c1c72a77eadbe91204b261
Aryan <itzaryan2015@gmail.com>
2026-05-26 00:13:22 +0530

    datatypes

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="aryan"
+let number=18
+let isloggedin=true
+let state;
+
+//sometypes of datatypes
+
+//number range->2^53
+//bigint
+//string
+//boolean
+//null-->standalone value(type of datatype->object)
+//undefined
+//symbol-> for uniques
+
+console.log(typeof null)//->Object
+
+console.log(typeof undefined)//->undefined
