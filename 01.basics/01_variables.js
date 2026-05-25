7984392c2da11dfd034911de07f502e4ba6881e2
Aryan <itzaryan2015@gmail.com>
2026-05-25 23:52:25 +0530

    variables basics

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,15 @@
+const account_id=133433
+let accountemail="aryan@123"
+var accountpassword="12345"
+ accountcity="jaipur"
+let accountstate;
+
+//  account_id=45 not allowed
+
+accountemail="rttgr@1122234"
+accountpassword="9776434"
+accountcity="nagpur"
+/*
+avoid var
+*/
+console.table([account_id,accountemail,accountpassword,accountcity,accountstate])
