a97c8a3b453dc3ac811beda3e4e47b3b74a60e3e
Aryan <itzaryan2015@gmail.com>
2026-05-27 13:24:59 +0530

    conversionofdattypes

---
+++ 01.basics/03_conversion_operations.js
@@ -0,0 +1,24 @@
+let score=true
+console.log(typeof score)//tells that it is of number type
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+ //"33"->33
+ //"33anc"->NaN(not  a number)
+ //true->1;false->0
+
+ let isloggedin=""
+ let boolencheck=Boolean(isloggedin)
+ console.log(boolencheck);
+ 
+//""->false->empty string gives false
+//"aryan"-> true->valid string gives true
+//1->true
+//0->flase
+let x=33
+let stringnumber=String(x)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
