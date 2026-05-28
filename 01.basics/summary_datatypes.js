06de86ecdcad2eb879f26b2d77986569bad3faa1
Aryan <itzaryan2015@gmail.com>
2026-05-28 17:33:05 +0530

    summary datatypes

---
+++ 01.basics/summary_datatypes.js
@@ -0,0 +1,8 @@
+//two types->primitive and non primitive
+//primitive->7 types
+//strings,Number,Boolean,null,undefined,symbol,Bingint
+
+
+
+//non primtive(reference types)
+//arrays,objects,Functions
