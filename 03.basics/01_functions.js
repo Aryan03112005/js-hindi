41ee7d0d68ec9877d2889a9f83f36c53819b4fef
Aryan <itzaryan2015@gmail.com>
2026-06-05 12:52:22 +0530

    functions

---
+++ 03.basics/01_functions.js
@@ -0,0 +1,72 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+// function calculatecartprice(...nums1){//...here is a rest operator that prints all the values without it ..this function will print only a the first value
+//     return nums1;
+// }
+// console.log(calculatecartprice(200,300,4565,45))//output->[ 200, 300, 4565, 45 ]
+function calculatecartprice(val1,val2,...nums1){//...here val1->200 and val2->300 gets in baki rest operator me 
+    return nums1;
+}
+console.log(calculatecartprice(200,300,4565,45))//output->[ 4565, 45 ]
+
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
