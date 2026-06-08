d388c2bd866b18d8b4005361f446a45d1fd9d6f3
Aryan <itzaryan2015@gmail.com>
2026-06-08 12:06:34 +0530

    foreach

--- 05.iterations/4.js
+++ 05.iterations/4.js
@@ -8,6 +8,26 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(myObject)
+    //console.log(myObject[key])
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(key);//this will preint only (indexes /key but in for of loop direct values were ggwtting printed)
+      console.log(programming[key])
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
+//so in for in maps are not directly iterable
